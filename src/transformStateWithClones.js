c0c741952e352f753c12eb704849f0e68706b9aa
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-20 18:50:47 +0100

    complete task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,45 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const changingState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(changingState, action.extraData);
+        stateHistory.push({ ...changingState });
+        break;
+
+      case 'removeProperties':
+        removeProperties(changingState, action.keysToRemove);
+        stateHistory.push({ ...changingState });
+        break;
+
+      case 'clear':
+        clear(changingState);
+        stateHistory.push({ ...changingState });
+        break;
+    }
+  }
+
+  return stateHistory;
+}
+
+function addProperties(state, extraData) {
+  Object.assign(state, extraData);
+}
+
+function removeProperties(state, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete state[key];
+  }
+}
+
+function clear(state) {
+  for (const key in state) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
